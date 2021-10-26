3ffabb2b32a99a02359a985b41282578f9894436
purikura09 <purikura09@gmail.com>
2021-10-26 23:06:37 +0200

    meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function handleResponse(response) {
-    console.log(response.data[0]);
+    //console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
+    //alert(`${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -23,6 +26,9 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <br />
+
+      <Results results={results} />
     </div>
   );
 }
