fcb1c82d02fb242f97f9d5f6560554c2330c71ed
purikura09 <purikura09@gmail.com>
2021-10-31 17:52:28 +0100

    fix bug

--- src/Photos.js
+++ src/Photos.js
@@ -14,6 +14,7 @@ export default function Photos(props) {
                     className="img-fluid"
                     key={index}
                     src={photo.src.landscape}
+                    alt="img"
                   />
                 </a>
               </div>
